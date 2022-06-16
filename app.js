40e0f6bf6028ba1734f1e985e1dc34dd7798ed6c
Varun-Pradeep <ascent817@gmail.com>
2022-06-16 13:01:20 -0500

    Add settings

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const root = document.getElementById("root");
 const container = document.getElementById("fun-container");
 
-let numTiles = 100;
+let numTiles = 256;
 
 for (let i = 0; i < (numTiles - 1); i++) {
     let tempNode = root.cloneNode(true);
@@ -27,26 +27,48 @@ function getCookie(cname) {
     return "";
 }
 
+function getUsername() {
+    if (getCookie('username') !== '') {
+        username = getCookie('username');
+    } else {
+        username = prompt("What's your name?");
+        let descision = prompt("Want a cookie?(y/n)");
+
+        while (descision !== "y" && descision !== "n") {
+            descision = prompt("You can only type \'y\' or \'n\'!");
+        }
+
+        if (descision === "y") {
+            alert("Great!");
+        } else {
+            alert("Too bad. Have a cookie anyway.");
+        }
+
+        document.cookie = `username=${username}`;
+    }
+}
+
 let username;
 
-if (getCookie('username') !== '') {
-    username = getCookie('username');
-} else {
-    username = prompt("What's your name?");
-    let descision = prompt("Want a cookie?(y/n)");
-
-    while (descision !== "y" && descision !== "n") {
-        descision = prompt("You can only type \'y\' or \'n\'!");
-    }
-
-    if (descision === "y") {
-        alert("Great!");
-    } else {
-        alert("Too bad. Have a cookie anyway.");
-    }
-
-    document.cookie = `username=${username}`;
-}
+getUsername();
 
 let messageElement = document.getElementById("message");
 messageElement.innerHTML = username + ", a cookie has been placed on your system.";
+
+let interval = 417;
+let waveDuration = 2000;
+let HTMLroot = document.documentElement;
+
+document.addEventListener("keydown", function (event) {
+    if (event.key == 'ArrowLeft') {
+        interval--;
+    } else if (event.key == 'ArrowRight') {
+        interval++;
+    } else if (event.key == 'w') {
+        waveDuration += 5;
+    } else if (event.key == 's') {
+        waveDuration -= 5;
+    }
+    HTMLroot.style.setProperty('--interval', interval + 'ms');
+    HTMLroot.style.setProperty('--wave-duration', waveDuration + 'ms');
+});
